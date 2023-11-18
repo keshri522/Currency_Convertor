13aa42ccdae46731ee7e216ad7673b801f3025e4
keshri522 <rkeshri522@gmail.com>
2023-11-18 17:58:01 +0530

    added sign up with google in register form for user convinninice and added api call to google

--- src/Components/Forms/RegisterForm.jsx
+++ src/Components/Forms/RegisterForm.jsx
@@ -1,6 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 const RegisterForm = ({ formdata, hanldeChange, SignupUser, loading }) => {
+  const navigate = useNavigate();
+  // this function will allow user to login or singup using the google auth
+  const login = useGoogleLogin({
+    // here token i dycrypeted if i want to get the details of user based on login i need to use jwt-dcode for this
+    onSuccess: async (tokenResponse) => {
+      try {
+        const res = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${tokenResponse.access_token}`,
+            },
+          }
+        );
+        // sending all the response which is user data to localStorage to access globally and also make the data persistent
+        // console.log(res); just for debugging
+        if (res.status === 200) {
+          // if wwill work only successful status code
+          localStorage.setItem("RegisterData", JSON.stringify(res.data));
+          navigate("/");
+        }
+      } catch (error) {
+        // console.log(error.message);
+        toast.error(error.message);
+      }
+    },
+  });
+
   // accepting the props here
   return (
     <div className="container">
@@ -76,7 +107,10 @@ const RegisterForm = ({ formdata, hanldeChange, SignupUser, loading }) => {
             </button>
           )}
           <div className="d-flex justify-content-between change mt-3">
-            <button className="btn btn-outline-warning w-100 p-1">
+            <button
+              onClick={() => login()}
+              className="btn btn-outline-warning w-100 p-1"
+            >
               <i className="fab fa-github icons"></i> Sign Up with Google
             </button>
           </div>
