104073444b2d487da22b71467f04ce98ad5ddeb0
keshri522 <rkeshri522@gmail.com>
2023-11-18 19:44:57 +0530

    .

--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -1,32 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-// import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import ToggleModeButton from "../Mode/ToggleMode";
-const Nav = () => {
-  const [boolean, Setboolean] = useState();
 
-  const gettingdata = () => {
-    let data = localStorage.getItem("loading");
-    if (data) {
-      Setboolean(data);
-    } else {
-      Setboolean("");
-    }
+const Nav = () => {
+  const navigate = useNavigate();
+  const [boolean, setBoolean] = useState(getDataFromLocalStorage());
+  // this function first get the data from local storage
+  function getDataFromLocalStorage() {
+    const data = localStorage.getItem("loading");
+    return data || "";
+  }
+
+  const handleStorageChange = () => {
+    setBoolean(getDataFromLocalStorage());
   };
+
   useEffect(() => {
-    gettingdata();
-  }, [boolean]);
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, [boolean]); // changes or run theuse effect based on boolean changes
+  const deleteUser = () => {
+    // To remove specific items from localStorage
+    localStorage.removeItem("RegisterData");
+    localStorage.removeItem("loading");
+    // after deeting redirects to home page
+    navigate("/");
+  };
 
   return (
     <>
-      <div className="container-fluid  p-3">
+      <div className="container-fluid p-3">
         <div className="row">
           <div className="col-md-12 d-flex justify-content-around">
             <ToggleModeButton></ToggleModeButton>
-            {/* conditionlly showing the login button based on localstroge but i can do this with hte help of redux useSelector hook  */}
-
             {boolean ? (
-              <Link className="nav-link" to="/login">
+              <Link onClick={deleteUser} className="nav-link" to="/login">
                 Logout
               </Link>
             ) : (
